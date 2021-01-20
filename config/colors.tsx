a3d512e645ba6e996e8bde1c9d0972ac4432406f
Daniel Arroyo <danielh989@gmail.com>
2021-01-20 11:44:19 -0300

    FIX: Color properties

--- config/colors.tsx
+++ config/colors.tsx
@@ -1,8 +1,8 @@
 export default {
-  primary: "424668",
-  secondary: "8B8FA6",
-  light: "E9EEF9",
-  dark: "737373",
-  danger: "CD2C32",
+  primary: "#424668",
+  secondary: "#8B8FA6",
+  light: "#E9EEF9",
+  dark: "#373737",
+  danger: "#CD2C32",
   white: "white",
 };
